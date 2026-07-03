f1089c44771f20562f8a93dfdc39e6b1d1a3f61b
Marek <marek.pessel@gmx.de>
2026-07-03 09:58:20 +0200

    func: video_from_csv

--- plot_utils.py
+++ plot_utils.py
@@ -1,24 +1,12 @@
+import pandas as pd
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
-from matplotlib.colors import to_rgb
 
 
 
-def fix_sim_step(size, creatures):
+def fix_sim_step(size, creatures, iteration):
     
-    # Hintergrund = weiß
-    grid = np.ones((size[0], size[1], 3), dtype=float)
-    creats = []
-    for creature in creatures:
-        row = creature.location[0]
-        col = creature.location[1]
-        color = creature.color[2:]
-        color = f"#{color}"
-
-        grid[row, col] = to_rgb(color)
-
-    #fig, ax = plt.subplots(figsize=(size[0], size[1]))
-    #ax.imshow(grid, interpolation="none")
     for creature in creatures:
         color = creature.color[2:]
         color = f"#{color}"
@@ -29,7 +17,6 @@ def fix_sim_step(size, creatures):
         c=color,                # Hex-Farbe
         marker="o",            # Kreis
         edgecolors="grey",
-        #figsize=(size[0], size[1])
     )
 
     # Always show the complete environment
@@ -38,7 +25,76 @@ def fix_sim_step(size, creatures):
     plt.gca().set_aspect("equal")
     plt.xticks([])
     plt.yticks([])
-    #plt.set_aspect("equal")
-    #ax.grid(True, color="grey", linewidth=0.5)
     
-    plt.show()
+    plt.savefig(f"test_plots/step_{iteration}.png")
+    #plt.show()
+
+
+def video_from_csv(csv_path, destination_path):
+    # CSV laden
+    df = pd.read_csv(csv_path, header=None)
+
+    # Weltgröße bestimmen
+    max_x = 0
+    max_y = 0
+
+    for _, row in df.iterrows():
+        for cell in row:
+            y, x, color = cell.split("_")
+            max_x = max(max_x, int(x))
+            max_y = max(max_y, int(y))
+
+    # Pixel pro Feld
+    scale = 8
+
+    WORLD_WIDTH = 128
+    WORLD_HEIGHT = 128
+
+    width = WORLD_WIDTH * scale
+    height = WORLD_HEIGHT * scale
+
+    fps = 30
+
+    video = cv2.VideoWriter(
+        destination_path,
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        fps,
+        (width, height)
+    )
+
+    for _, row in df.iterrows():
+
+        # schwarzer Hintergrund
+        frame = np.full((height, width, 3), 255, dtype=np.uint8)
+
+        for cell in row:
+
+            y, x, color = cell.split("_")
+
+            x = int(x)
+            y = int(y)
+
+            # Hex-Farbe nach RGB
+            rgb = int(color, 16)
+
+            r = (rgb >> 16) & 255
+            g = (rgb >> 8) & 255
+            b = rgb & 255
+
+            # OpenCV verwendet BGR
+            bgr = (b, g, r)
+
+            # Quadrat zeichnen
+            cv2.circle(
+                frame,
+                (x * scale + scale // 2, y * scale + scale // 2),
+                scale // 2,
+                bgr,
+                -1
+            )
+
+        video.write(frame)
+
+    video.release()
+
+    print("Video gespeichert.")
